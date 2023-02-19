441b40aa5e7ef08520df78722a168355d5d7f5b2
Dmitry <banindm@mail.ru>
2023-02-19 16:16:54 +0530

    contact-page adaptive

--- src/pages/contact-page/contact-page.tsx
+++ src/pages/contact-page/contact-page.tsx
@@ -1,42 +1,37 @@
 import './contact-page-style.css';
-import { Fragment } from 'react';
 
 function ContactPage(): JSX.Element {
   return (
-    <Fragment>
-      <section className='contact__container'>
-        <div className='contact'>
-          <h1 className='contact__title'>Contact Us</h1>
-          <div className='contact__info-box'>
-            <div className='contact__info'>
-              <h2 className='contact__info-title'>Email</h2>
-              <div className='border'></div>
-              <div className='contact__email'>
-                <h3 className='contact__headline headline-text'>
-                  INVESTMENT INQUIRIES
-                </h3>
-                <div>
-                  <p className='contact__desc'>
-                    You can email our Capital Formation team at:{' '}
-                  </p>
-                  <a href='mailto:narek.akopyan.j@gmail.com'>
-                    narek.akopyan.j@gmail.com
-                  </a>
-                </div>
+    <section className='contact__container'>
+      <div className='contact'>
+        <h1 className='contact__title'>Contact Us</h1>
+        <div className='contact__info-box'>
+          <div className='contact__info contact__info-email'>
+            <h2 className='contact__info-title'>Email</h2>
+            <div className='contact__email'>
+              <h3 className='contact__headline headline-text'>
+                INVESTMENT INQUIRIES
+              </h3>
+              <div>
+                <p className='contact__desc'>
+                  You can email our Capital Formation team at:
+                </p>
+                <a href='mailto:narek.akopyan.j@gmail.com'>
+                  narek.akopyan.j@gmail.com
+                </a>
               </div>
             </div>
-            <div className='contact__info'>
-              <h3 className='contact__info-title'>Locations</h3>
-              <div className='border'></div>
-              <ul className='country__headline headline-text'>
-                <li className='country'>ARMENIA</li>
-                <li className='country'>ESTONIA</li>
-              </ul>
-            </div>
+          </div>
+          <div className='contact__info contact__info-location'>
+            <h3 className='contact__info-title'>Locations</h3>
+            <ul className='country__headline headline-text'>
+              <li className='country'>ARMENIA</li>
+              <li className='country'>ESTONIA</li>
+            </ul>
           </div>
         </div>
-      </section>
-    </Fragment>
+      </div>
+    </section>
   );
 }
 
